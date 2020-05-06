5137b32a8b060faacd5b7b4f5672e9dc35b3c04a
Ryan Hartje <hartjepc@gmail.com>
2020-05-05 20:52:30 -0500

    create tilemapping system to draw out maps

--- level.go
+++ level.go
@@ -8,13 +8,25 @@ import (
 type Level struct {
 	// BGFile is the filepath to the background
 	BGFile  string
-	Texture *sdl.Texture
 	Sounds  map[string][]*mix.Chunk
+	Texture *sdl.Texture
+	// TileMap is a matrix representing the map
+	// TileMap[x][y]
+	TileMap map[int]map[int]Tile
+}
+
+// Tile represents a tile in a tilemap. This might be a 16x16 sprite or a 16x128 tile.
+type Tile struct {
+	x0 int32
+	x1 int32
+	y0 int32
+	y1 int32
 }
 
 // NewLevel takes in the filepath of a level's background, and a renderer
 func NewLevel(filepath string, renderer *sdl.Renderer) (*Level, error) {
 	img, err := sdl.LoadBMP(filepath)
+
 	checkErr(err)
 	defer img.Free()
 
--- main.go
+++ main.go
@@ -7,6 +7,11 @@ import (
 	"github.com/veandco/go-sdl2/sdl"
 )
 
+const (
+	winW = 800
+	winH = 600
+)
+
 var (
 	fonts = []string{"fonts/monogram.ttf"}
 )
@@ -16,11 +21,11 @@ func main() {
 	e.Init()
 
 	window, err := sdl.CreateWindow(
-		"GoGome",
+		"GoGnome",
 		sdl.WINDOWPOS_UNDEFINED,
 		sdl.WINDOWPOS_UNDEFINED,
-		800,
-		600,
+		winW,
+		winH,
 		sdl.WINDOW_OPENGL,
 	)
 	checkErr(err)
@@ -35,8 +40,19 @@ func main() {
 	player, err := NewPlayer(renderer)
 	checkErr(err)
 
-	level, err := NewLevel("sprites/background.bmp", renderer)
+	// Load in our level asset and generate a plain map
+	level, err := NewLevel("sprites/overworld.bmp", renderer)
 	checkErr(err)
+	grass := Tile{x0: 0, x1: 16, y0: 0, y1: 16}
+	mapping := map[int]map[int]Tile{}
+	// Generate a plain grass map
+	for x := 0; x < winW; x += 16 {
+		mapping[x] = make(map[int]Tile)
+		for y := 0; y < winH; y += 16 {
+			mapping[x][y] = grass
+		}
+	}
+	level.TileMap = mapping
 
 	if err := mix.OpenAudio(44100, mix.DEFAULT_FORMAT, 2, 4096); err != nil {
 		checkErr(err)
@@ -62,14 +78,28 @@ func main() {
 			case *sdl.QuitEvent:
 				os.Exit(0)
 			}
+			// Setup ESC to exit keybinding
+			keys := sdl.GetKeyboardState()
+			if keys[sdl.SCANCODE_ESCAPE] == 1 {
+				os.Exit(0)
+			}
 		}
 
-		// Render the background of the level
-		renderer.Copy(
-			level.Texture,
-			&sdl.Rect{X: 0, Y: 0, W: 800, H: 600},
-			&sdl.Rect{X: 0, Y: 0, W: 800, H: 600},
-		)
+		// Render tilemap
+		for x := 0; x < winW; x += 16 {
+			for y := 0; y < winH; y += 16 {
+				tile := level.TileMap[x][y]
+				width := tile.x1 - tile.x0
+				height := tile.y1 - tile.y0
+				// Render the background of the level
+				renderer.Copy(
+					level.Texture,
+					&sdl.Rect{X: tile.x0, Y: tile.y0, W: width, H: height},
+					&sdl.Rect{X: int32(x), Y: int32(y), W: width, H: height},
+				)
+			}
+		}
+
 		enemy.Draw()
 		enemy.Update()
 		//renderer.Copy(nil, &text.ClipRect, &sdl.Rect{X: 400, Y: 400, W: 100, H: 36})
