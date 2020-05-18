0cbe5be349b0482c5470d39f89fac45f579e4c78
Ryan Hartje <hartjepc@gmail.com>
2020-05-18 13:38:49 -0500

    partially resolved x and y edge case rendering. Updating grid logic to draw to level's TileSize

--- pkg/engine/level.go
+++ pkg/engine/level.go
@@ -25,8 +25,10 @@ type Level struct {
 	Sounds      map[string][]*mix.Chunk
 	Texture     *sdl.Texture
 	// TileMap is a matrix representing the map
+	// A z index is arbitrarily establish by the slice ordering
+	// the 0th object is drawn first, with each subsequent object drawn on top of the previous one
 	// TileMap[x][y]
-	TileMap  map[int]map[int]Tile
+	TileMap  map[int]map[int][]Tile
 	TileSize int
 	// Current level's coordinates
 	X, Y int
@@ -90,20 +92,26 @@ func NewRandomizedLevel(filepath string, renderer *sdl.Renderer) (*Level, error)
 
 	grass := Tile{Name: "grass", X0: 0, X1: 16, Y0: 0, Y1: 16}
 	grass2 := Tile{Name: "grass2", X0: 272, X1: 303, Y0: 464, Y1: 495}
-	mapping := map[int]map[int]Tile{}
+	bush := Tile{Name: "bush", X0: 32, X1: 48, Y0: 224, Y1: 240}
+	mapping := map[int]map[int][]Tile{}
 	level.EntityMap = map[int]map[int]Entity{}
 
 	// Bootstrap TileMap for the background, and entity map to render entities on top of
 	// iterate by the x and y values of the sprite's width and height, so that you don't
 	// draw over other tiles.
-	for x := 0; x < (winW * 10); x += 32 {
-		mapping[x] = make(map[int]Tile)
+	for x := 0; x < (winW * 10); x += level.TileSize {
+		mapping[x] = make(map[int][]Tile)
 		level.EntityMap[x] = make(map[int]Entity)
-		for y := 0; y < (winH * 10); y += 32 {
-			mapping[x][y] = grass
+		for y := 0; y < (winH * 10); y += level.TileSize {
+			// populate map with Tiles
+			mapping[x][y] = []Tile{}
+			mapping[x][y] = append(mapping[x][y], grass)
 			// half the time, give us different grass
 			if rand.Intn(10) > 5 {
-				mapping[x][y] = grass2
+				mapping[x][y] = append(mapping[x][y], grass2)
+			}
+			if rand.Intn(10) == 0 {
+				mapping[x][y] = append(mapping[x][y], bush)
 			}
 			level.EntityMap[x][y] = nil
 		}
@@ -118,46 +126,74 @@ func (level *Level) Draw(renderer *sdl.Renderer) {
 	// Render level to window tile by tile
 	for x := 0; x < level.CameraX; x += level.ScrollSpeed {
 		for y := 0; y < level.CameraY; y += level.ScrollSpeed {
-			// default tile placement
-			tile := level.TileMap[level.X+x][level.Y+y]
-			width := tile.X1 - tile.X0
-			height := tile.Y1 - tile.Y0
-
-			// Render the background of the level
-			renderer.Copy(
-				level.Texture,
-				&sdl.Rect{X: tile.X0, Y: tile.Y0, W: width, H: height},
-				&sdl.Rect{X: int32(x), Y: int32(y), W: width * 2, H: height * 2},
-			)
-
-			// on x=0 and y=0, we need to render partials on the left side of the screen when scrolling
-			// we're using 16 here because that's the prerendered tile's size (16x16)
-			// This will need to be refactored to support dynamically sized tiles
-			if x == 0 && level.X%16 != 0 {
-				xOffset := level.X % 16
-
-				tile := level.TileMap[level.X-xOffset][level.Y+y]
-				width := int32(xOffset)
+			tiles := level.TileMap[level.X+x][level.Y+y]
+			for _, tile := range tiles {
+				width := tile.X1 - tile.X0
 				height := tile.Y1 - tile.Y0
 
 				// Render the background of the level
 				renderer.Copy(
 					level.Texture,
-					&sdl.Rect{X: tile.X0 + int32(xOffset), Y: tile.Y0, W: width, H: height},
+					&sdl.Rect{X: tile.X0, Y: tile.Y0, W: width, H: height},
 					&sdl.Rect{X: int32(x), Y: int32(y), W: width * 2, H: height * 2},
 				)
-				if y < 200 {
-					fmt.Printf("Rendering %d,%d at level %d,%d from pixel %d,%d\t ", x, y, level.X, level.Y, tile.X0+int32(xOffset), tile.Y0)
-					fmt.Printf("Using TileMap[%d][%d]: %s\t", level.X-xOffset, level.Y+y, level.TileMap[level.X-xOffset][level.Y+y].Name)
-					fmt.Printf("Placed at %d,%d w: %d h: %d\n", x, y, width, height)
-				}
 			}
 
+			// on x=0 and y=0, we need to render partials on the left side of the screen when scrolling
+			// we're using 16 here because that's the prerendered tile's size (16x16)
+			// This will need to be refactored to support dynamically sized tiles
+			// TODO - TileSize handling is VERY fragile, This should be reworked/redesigned to be more resilient
+			if x == 0 && level.X%level.TileSize != 0 {
+				xOffset := level.X % level.TileSize
+				halfSet := xOffset / 2 // half set is used to determine where on the texture map to find the asset
+
+				tiles := level.TileMap[level.X-xOffset][level.Y+y]
+				for _, tile := range tiles {
+					width := level.TileSize - xOffset
+					height := tile.Y1 - tile.Y0
+
+					// Render the background of the level
+					renderer.Copy(
+						level.Texture,
+						&sdl.Rect{X: tile.X0 + int32(halfSet), Y: tile.Y0, W: int32(width), H: height},
+						&sdl.Rect{X: int32(x), Y: int32(y), W: int32(width) * 2, H: height * 2},
+					)
+					if y < 32 {
+						fmt.Printf("Rendering %d,%d at level %d,%d from pixel %d,%d\t ", x, y, level.X, level.Y, tile.X0+int32(xOffset), tile.Y0)
+						fmt.Printf("Using TileMap[%d][%d]: %v\t", level.X-xOffset, level.Y+y, level.TileMap[level.X-xOffset][level.Y+y])
+						fmt.Printf("Placed at %d,%d w: %d h: %d\n", x, y, width, height)
+					}
+				} // end of tiles for loop
+			} // end if for x == 0 edge case
+
+			// on x=0 and y=0, we need to render partials on the left side of the screen when scrolling
+			// we're using 16 here because that's the prerendered tile's size (16x16)
+			// This will need to be refactored to support dynamically sized tiles
+			if y == 0 && level.Y%level.TileSize != 0 {
+				yOffset := level.Y % level.TileSize
+				halfSet := yOffset / 2
+
+				tiles := level.TileMap[level.X+x][level.Y-yOffset]
+				for _, tile := range tiles {
+					width := tile.X1 - tile.X0
+					height := level.TileSize - yOffset
+
+					// Render the background of the level
+					renderer.Copy(
+						level.Texture,
+						&sdl.Rect{X: tile.X0, Y: tile.Y0 + int32(halfSet), W: width, H: int32(height)},
+						&sdl.Rect{X: int32(x), Y: int32(y), W: width * 2, H: int32(height) * 2},
+					)
+				} // end of tiles for loop
+			} // end if for y == 0 edge case
+
 			if level.Debug {
-				// draw vertical grid lines
-				gfx.LineRGBA(renderer, int32(x), 0, int32(x), int32(winH), 100, 0, 0, 100)
-				// draw horizontal line
-				gfx.LineRGBA(renderer, 0, int32(y), int32(winW), int32(y), 100, 0, 0, 100)
+				if x%level.TileSize == 0 && y%level.TileSize == 0 {
+					// draw vertical grid lines
+					gfx.LineRGBA(renderer, int32(x), 0, int32(x), int32(winH), 100, 0, 0, 100)
+					// draw horizontal line
+					gfx.LineRGBA(renderer, 0, int32(y), int32(winW), int32(y), 100, 0, 0, 100)
+				}
 			}
 		}
 	}
